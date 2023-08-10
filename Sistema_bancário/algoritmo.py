5d1f19d808209cab79dfa57d46fea9fd1653583b
Anjosisa <isahanjos1801@gmail.com>
2023-08-10 15:44:47 -0300

    criação do menu

--- Sistema_bancário/algoritmo.py
+++ Sistema_bancário/algoritmo.py
@@ -1,11 +1,23 @@
 from classes import *
+import os 
 
 def main ():
     while True:
         try:
-            print("Você está no banco tal, para entrar na sua conta, digite sue nome: ")
-            nome = input("Digite seu nome aqui:")
+            print("Você está no banco tal, você é: \n1 - cliente\n2 - admiistrador ")
+            menu = input("Digite uma das opções acima: ")
+
+            if menu == 1:
+                 print("Você entrou em clientes, entre na sua conta: ")
+                 os.system("pause")
+                 os.system("cls")
+
+            elif menu == 2:
+                 print("você entrou em administrador, oq que deseja fazer?")
+                 os.system("pause")
+                 os.system("cls")
             
+
 
 
         except Exception as erro: # Vai captar qualquer erro ou qualquer anormalidade
