4b84db0384aaaf7019ef4609556bc9d0a0365dbf
Anjosisa <isahanjos1801@gmail.com>
2023-08-10 14:32:47 -0300

    Inicio do while

--- Sistema_bancário/algoritmo.py
+++ Sistema_bancário/algoritmo.py
@@ -1,1 +1,11 @@
 from classes import *
+
+def main ():
+    while True:
+        try:
+            
+
+
+        except Exception as erro: # Vai captar qualquer erro ou qualquer anormalidade
+		print("Um erro foi detectado. Modifique para que seu software funcione corretamente")
+		print(erro.__class.__name)
