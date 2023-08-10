7e0a3c18dd5f6627323e1dd410eb7e2ab444383a
natinhaaa <natvcer2@gmail.com>
2023-08-10 14:44:14 -0300

    update

--- Sistema_bancário/algoritmo.py
+++ Sistema_bancário/algoritmo.py
@@ -3,9 +3,15 @@ from classes import *
 def main ():
     while True:
         try:
+
+            #def adicionar():
+                #print("~Para adicionar uma nova conta, é preciso as seguintes informações:")
+                #nome = Clientes(input("Insira seu nome: "))
+                #rg = Clientes(input("Insira seu RG: "))
+                #cep = Clientes(input("Insira o seu CEP: "))
             
 
 
         except Exception as erro: # Vai captar qualquer erro ou qualquer anormalidade
-		print("Um erro foi detectado. Modifique para que seu software funcione corretamente")
-		print(erro.__class.__name)
+        print("Um erro foi detectado. Modifique para que seu software funcione corretamente")
+        print(erro.__class.__name)
