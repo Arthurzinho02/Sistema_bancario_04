ac5ebce9d882e66e87aa1d49de8d5b070d250c87
natinhaaa <natvcer2@gmail.com>
2023-08-10 14:09:05 -0300

    update

---
+++ Sistema_bancário/classes.py
@@ -0,0 +1,18 @@
+class Clientes:
+    def __init__ (self, nome, rg, cep):
+        self.nome = nome
+        self.rg = rg
+        self.cep = cep
+
+class Banco_Brasil:
+    def adicionar_clientes():
+    
+    def excluir_clientes():
+
+    def atualizar_clientes():
+    
+    def transferência():
+
+    def saque():
+
+    def depósito():
