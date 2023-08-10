b57390efc7eb3740ecec5ee5e7f732b6a508383b
Malaquia24 <arthur.matheus0607@gmail.com>
2023-08-10 15:55:07 -0300

    update

--- Sistema_bancário/classes.py
+++ Sistema_bancário/classes.py
@@ -1,32 +1,35 @@
 
 class Clientes:
-    def __init__ (self, nome, rg, cep):
+    def __init__ (self, nome, rg, cep, saldo):
         self.nome = nome
         self.rg = rg
         self.cep = cep
-        
-class conta:
-    def __init__(self, saldo):
         self.saldo = saldo
+
+    # def transferência(self):
+
+    # def saque(self):
+
+    # def depósito(self):
+
+    
 
 class Banco_Brasil:
 
     def __init__(self):
         self._lista = []
         
-    def adicionar_clientes(self, nome, rg, cep):
+    def adicionar_clientes(self, nome, rg, cep, saldo):
         cliente = Clientes(nome, rg, cep)
         self._lista.append(cliente)
         
-    def excluir_clientes(self, ):
-        self._lista.pop()
+    def excluir_clientes(self):
+        for cliente in self._lista:
+            self._lista(cliente.nome)
+
+
         
 
-    def atualizar_clientes(self):
+    # def atualizar_clientes(self):
     
-    def transferência(self):
-
-    def saque(self):
-
-    def depósito(self):
 
