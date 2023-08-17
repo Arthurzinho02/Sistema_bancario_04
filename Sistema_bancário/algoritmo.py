ab94bcc65cdaaf45e874ceccde68c9bcd798aead
Anjosisa <isahanjos1801@gmail.com>
2023-08-17 07:41:15 -0300

    Algoritmo 17/08

--- Sistema_bancário/algoritmo.py
+++ Sistema_bancário/algoritmo.py
@@ -3,19 +3,47 @@ import os
 
 def main ():
     while True:
-            print("Você está no banco tal, você é: \n1 - cliente\n2 - admiistrador ")
-            menu = input("Digite uma das opções acima: ")
+            try:
+               print("Você está no banco tal, você é: \n1 - cliente\n2 - admiistrador ")
+               
+               def menu_p():
+                    input("Digite uma das opções acima: ")
 
-            if menu == 1:
-                 print("Você entrou em clientes, entre na sua conta: ")
-                 os.system("pause")
-                 os.system("cls")
+                    if menu_p == 1:
+                         print("Você entrou em clientes, entre na sua conta: ")
+                         os.system("pause")
+                         os.system("cls")
 
-            elif menu == 2:
-                 print("você entrou em administrador, oq que deseja fazer?")
-                 os.system("pause")
-                 os.system("cls")
-            
+                    elif menu_p == 2:
+                         print("você entrou em administrador, oq que deseja fazer?")
+                         os.system("pause")
+                         os.system("cls")
+
+               print ("Você entrou na opção cliente, o que deseja realizar? \n1 - Transferência entre clientes\n2 - Saque\n3 - Depósito ")
+               
+               def menu_c():
+                    input ("Digite o que precisa ser realizado: ")
+
+                    if menu_c == 1:
+                         print ("Você entrou em Tranferência entre clientes, digite qual valor será transferido: ")
+                         os.system("pause")
+
+                    elif menu_c == 2:
+                         print ("Você está em Saque, digite o valor que deseja retirar: ")
+                         os.system("pause")
+
+                    elif menu_c == 3:
+                         print("Você entrou em Depósitos, digite a quantia que deseja depositar: ")
+                    
+                    elif menu_c == 4:
+                         print("Saindo do programa.")
+                         break
+     
+
+
+          
+                 
+          
 
 
 
