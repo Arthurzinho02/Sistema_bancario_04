cd54ee6a0beb40eac5f8b2e79e922fdc19789782
Arthurzinho02 <arthur.matheus0607@gmail.com>
2023-10-01 12:46:50 -0300

    update

--- Sistema_bancário/algo.py
+++ Sistema_bancário/algo.py
@@ -3,7 +3,7 @@ import os
 
 def Main():
     banco = Banco_Brasil()
-    contID = 0
+    contID = 00
     while True:
         try:
             print("Você está no banco do Brasil!")
@@ -14,90 +14,163 @@ def Main():
 
             match opção:
                 case 1:
-                    print("Esse é o Menu destinado aos Clientes")
-                    print("1. Transferência\n2. Saque\n3. Depósito")
-                    função = input("O que você deseja fazer? ")
+                    print("===Login===")
+                    nome_L = input("Digite seu nome:").upper()
+                    id_L = int(input("Digite seu id: "))
                     os.system("cls")
+                    match banco.loginCliente(id_L, nome_L):
+                        case 1:
+                            while True:
+                                print(f"===Bem vindo {banco.getnome(id_L)}===")
+                                print(f"Saldo disponível: {banco.getsaldo(id_L)}")
+                                print("1. Transferência\n2. Saque\n3. Depósito \n4. Sair")
+                                função = int(input("O que você deseja fazer? "))
+                                os.system("cls")
+                                match função:
+                                    case 1:
+                                        print("Essa é a área de TRANSFERÊNCIA")
+                                        destinatário = int(input("Digite o ID do destinário que você deseja enviar o valor: "))
+                                        valor = float(input("Digite o valor que deseja transferir: "))
+                                        banco.transferencia(id_L, destinatário, valor)
+                                        os.system("pause")
+                                        os.system("cls")
+                                
+                                    case 2:
+                                        print("Essa é a área de SAQUE")
+                                        valor = float(input("Digite o valor que deseja sacar: "))
+                                        banco.saque(id_L, valor)
+                                        os.system("pause")
+                                        os.system("cls")
+                                
+                                    case 3:
+                                        print("Essa é a área de DEPÓSITO")
+                                        valor = float(input("Digite o valor que deseja depositar: "))
+                                        banco.depósito(id_L, valor)
+                                        os.system("pause")
+                                        os.system("cls")
 
-                    if função == "1":
-                        print("Essa é a área de TRANSFERÊNCIA")
-                        destinatário = int(input("Digite o ID do destinário que você deseja enviar o valor: "))
-                        valor = float(input("Digite o valor que deseja transferir: "))
-                        banco.transferencia(destinatário, valor)
-                        os.system("pause")
-                    
-                    elif função == "2":
-                        print("Essa é a área de SAQUE")
-                        valor = float(input("Digite o valor que deseja sacar: "))
-                        banco.saque(valor)
-                        os.system("pause")
-                    
-                    elif função == "3":
-                        print("Essa é a área de DEPÓSITO")
-                        valor = float(input("Digite o valor que deseja depositar: "))
-                        banco.depósito(valor)
-                        os.system("pause")
+                                    case 4:
+                                        break
 
-                    else:
-                        print("A opção não existe")
-                        os.system("pause")
-                        os.system("cls")
-
+                                    case _:
+                                        print("A opção não existe")
+                                        os.system("pause")
+                                        os.system("cls")
+                        case _:
+                            print("Id ou nome incorretos")
+                            os.system("pause")
+                            os.system("cls")
                 case 2:
-                    print("Esse é o Menu destinado ao Administrador")
-                    print("1. Criar conta\n2. Excluir conta\n3. Atualizar dados")
-                    função = input("O que você deseja fazer? ")
+                    print("===Login ADM===")
+                    nome_A = input("Digite seu nome: ").upper()
+                    senha_A = int(input("Digite sua senha: "))
                     os.system("cls")
+                    match banco.LoginAdm(nome_A, senha_A):
+                        case 1:
+                            while True:
+                                print(f"===Bem vindo {nome_A}===")
+                                print("1. Criar Cliente \n2. Listar Clientes \n3. Excluir Cliente \n4. Atualizar Dados \n5. Criar ADM \n6. Listar \n7. Excluir ADM \n8. Sair")
+                                função = int(input("O que você deseja fazer? "))
+                                os.system("cls")
+                                match função:
 
-                    if função == "1":
-                        print("Essa é a área de CRIAR CONTA")
-                        contID += 1
-                        nome = input("Nome do usuário: ")
-                        rg = int(input("RG: "))
-                        cep = int(input("CEP: "))
-                        id = contID
-                        banco.adicionar_clientes(nome, rg, cep, id)
-                        os.system("pause")
-                    
-                    elif função == "2":
-                        print("Essa é a área de EXCLUIR CONTA")
-                        conta = int(input("Qual ID da conta você deseja excluir? "))
-                        banco.excluir_clientes(conta)
-                        os.system("pause")
-                    
-                    elif função == "3":
-                        print("Essa é a área de ATUALIZAR DADOS\nO que você deseja atualizar?\n1 - Nome\n2 - RG\n3 - CEP")
-                        conta = input("Qual item você deseja atualizar? ")
-                        banco.autalizar(conta)
-                        os.system("pause")
+                                    case 1:
+                                        print("Essa é a área de CRIAR CONTA")
+                                        contID += 1
+                                        nome = input("Nome do usuário: ").upper()
+                                        rg = int(input("RG: "))
+                                        cep = int(input("CEP: "))
+                                        banco.adicionar_clientes(nome, rg, cep, contID)
+                                        os.system("pause")
+                                        os.system("cls")
+                                    
+                                    case 2:
+                                        print("===Lista de Clientes===")
+                                        banco.listarClientes()
+                                        os.system("pause")
+                                        os.system("cls")
 
-                        if função == 1:
-                            NOME = input("Digite um novo nome")
-                            banco.setnome(conta, NOME)
+                                    case 3:
+                                        print("===EXCLUIR CONTA===\n")
+                                        banco.listarClientes()
+                                        conta = int(input("\nQual ID da conta você deseja excluir? "))
+                                        banco.excluir_clientes(conta)
+                                        os.system("pause")
+                                        os.system("cls")
+                                    
+                                    case 4:
+                                        print("===ATUALIZAR DADOS=== \nO que você deseja atualizar? \n1 - Nome \n2 - RG \n3 - CEP")
+                                        conta = input("Qual item você deseja atualizar? ")
+                                        os.system("pause")
+                                        os.system("cls")
+
+                                        match conta:
+
+                                            case 1:
+                                                nome_N = input("Digite um novo nome").upper
+                                                banco.setnome(conta, nome_N)
+                                                print("Novo nome registrado")
+                                                os.system("pause")
+                                                os.system("cls")
+
+                                            case 2:
+                                                rg_N = input("Digite um novo RG")
+                                                banco.setrg(conta, rg_N)
+                                                print("Novo RG registrado")
+                                                os.system("pause")
+                                                os.system("cls")
+
+                                            case 3:
+                                                cep_N = input("Digite um novo cep")
+                                                banco.setcep(conta, cep_N)
+                                                print("Novo CEP registrado")
+                                                os.system("pause")
+                                                os.system("cls")
+                                            
+                                            case _:
+                                                print("A opção não existe")
+                                                os.system("pause")
+                                                os.system("cls")
+
+                                    case 5:
+                                        print("===Criar ADM==")
+                                        nome_AD = input("Digite o nome: ").upper()
+                                        senha_AD = int(input("Digite a senha: "))
+                                        banco.adicionarAdm(nome_AD, senha_AD)
+                                        print("ADM adicionado")
+                                        os.system("pause")
+                                        os.system("cls")
+
+                                    case 6:
+                                        print("===Lista de AMDs===")
+                                        banco.listarAdms()
+                                        os.system("pause")
+                                        os.system("cls")
+
+                                    case 7:
+                                        print("===Excluir ADM===")
+                                        banco.listarAdms()
+                                        vetor = int(input("Digite o índice do ADM a ser excluido: "))
+                                        banco.excluiradms(vetor)
+                                        print("ADM excluido")
+                                        os.system("pause")
+                                        os.system("cls")
+
+                                    case 8:
+                                        break
+                                    case _:
+                                        print("A opção não existe")
+                                        os.system("pause")
+                                        os.system("cls")
+                        case _:
+                            print("Nome ou senha incorretos")
                             os.system("pause")
-
-                        elif função == "2":
-                            RG = input("Digite um novo RG")
-                            banco.setrg(conta, RG)
-                            os.system("pause")
-
-                        elif função == "3":
-                            CEP = input("Digite um novo cep")
-                            banco.setcep(conta, CEP)
-                            os.system("pause")
-                        
-                        else:
-                            print("A opção não existe")
-                            os.system("pause")
-
-                    else:
-                        print("A opção não existe")
-                        os.system("pause")
-                        os.system("cls")
+                            os.system("cls")
 
                 case _:
                     print("Opção Invalida")
                     os.system("pause")
+                    os.system("cls")
 
         except Exception as erro:
             print("Valor invalido")
--- Sistema_bancário/classes.py
+++ Sistema_bancário/classes.py
@@ -5,23 +5,89 @@ class Clientes:
         self.cep = cep
         self.saldo = saldo
 
+    def getSaldo(self):
+        return self.saldo
+    
+    def setSaldo(self, x):
+        self.saldo = x
+     
+    def getNome(self):
+        return self.nome
+    
+    def setNome(self, x):
+        self.nome = x
+    
+    def getCep(self):
+        return self.cep
+    
+    def setCep(self, x):
+        self.cep = x
+    
+    def getRg(self):
+        return self.rg
+    
+    def setRg(self, x):
+        self.rg = x
+    
+
 class Banco_Brasil:
 
     def __init__(self):
         self.dicionario = {}
+        self.ADM = ADM("MASTER", 123)
+        self.listaAdm = [self.ADM]
         
-    def adicionar_clientes(self, nome, rg, cep, id, saldo=0):
+
+    def listarAdms(self):
+        cont = 0
+        for i in self.listaAdm:
+            cont += 1
+            print(f"{cont}- Nome: {i.getNomeA()}")
+    
+    def excluiradms(self, vetor):
+        self.listaAdm.pop(vetor - 1)
+
+    def adicionarAdm(self, nome, senha):
+        adm = ADM(nome, senha)
+        self.listaAdm.append(adm)
+
+    def LoginAdm(self, nome, senha):
+        for i in self.listaAdm:
+            if i.getNomeA() == nome and i.getSenhaA() == senha:
+                return 1          
+        
+
+    def loginCliente(self, id, nome):
+        for chave, valor in self.dicionario.items():
+            if chave == id and valor.getNome() == nome:
+                return 1
+        
+    def getCliente(self, id):
+        for chave, valor in self.dicionario.items():
+            if chave == id:
+                valor.getSaldo()
+
+
+    def adicionar_clientes(self, nome, rg, cep, id):
+        saldo = 0
         cliente = Clientes(nome, rg, cep, saldo)
         self.dicionario[id] = cliente
         print (f"{nome} adicionado, o id é {id}")
+
+
+    def listarClientes(self):
+        cont = 0
+        for chave, valor in self.dicionario.items():
+            cont += 1
+            print(f"{cont}- Nome: {valor.getNome()} | Id: {chave} | RG: {valor.getRg()} | CEP: {valor.getCep()} ")
         
     def excluir_clientes(self, id):
         p = 0
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
+                print(f"{valor.getNome()} excluido")
                 self.dicionario.pop(chave)
-                print(f"{valor.nome} excluido")
                 break            
         if p != 1:
             print("Cliente não encontrado")
@@ -31,7 +97,7 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                return valor.nome
+                return valor.getNome()
         if p != 1:
             print("Cliente não encontrado")
     
@@ -40,8 +106,8 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                valor.nome = x
-                print(f"O nome novo é {valor.nome}")
+                valor.setNome(x)
+                print(f"O nome novo é {valor.getNome()}")
                 break
         if p != 1:
             print("Cliente não encontrado")
@@ -51,7 +117,7 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                return valor.rg
+                return valor.getRg()
         if p != 1:
             print("Cliente não encontrado")
 
@@ -61,8 +127,8 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                valor.rg = x
-                print(f"O RG novo é {valor.rg}")
+                valor.setRg(x)
+                print(f"O RG novo é {valor.getRg()}")
                 break
         if p != 1:
             print("Cliente não encontrado")
@@ -72,7 +138,7 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                return valor.cep
+                return valor.getCep()
         if p != 1:
             print("Cliente não encontrado")
 
@@ -82,8 +148,8 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                valor.cep = x
-                print(f"O CEP novo é {valor.cep}")
+                valor.setCep(x)
+                print(f"O CEP novo é {valor.getCep()}")
                 break
         if p != 1:
             print("Cliente não encontrado")
@@ -93,7 +159,7 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                return valor.saldo
+                return valor.getSaldo()
         if p != 1:
             print("Cliente não encontrado")
 
@@ -102,8 +168,9 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                valor.saldo = valor.saldo + x
-                print(f"O Valor do saldo atual é de: R${valor.saldo}")
+                x = valor.getSaldo() + x
+                valor.setSaldo(x)
+                print(f"O Valor do saldo atual é de: R${valor.getSaldo()}")
                 break
         if p != 1:
             print("Cliente não encontrado")
@@ -113,9 +180,10 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                if valor.saldo >= 0 and x <= valor.saldo:    
-                    valor.saldo = valor.saldo - x
-                    print(f"O Valor do saldo atual é de: R${valor.saldo}, você sacou {x}")
+                if valor.getSaldo() >= 0 and x <= valor.getSaldo():    
+                    x1 = valor.getSaldo() - x
+                    valor.setSaldo(x1)
+                    print(f"O Valor do saldo atual é de: R${valor.getSaldo()}, você sacou {x}")
                     break
                 else:
                     print("Saldo insuficiente")                
@@ -127,21 +195,35 @@ class Banco_Brasil:
         for chave, valor in self.dicionario.items():
             if chave == id:
                 p = 1
-                if valor.saldo >= 0 and x <= valor.saldo:    
-                    valor.saldo = valor.saldo - x
+                if valor.getSaldo() >= 0 and x <= valor.getSaldo():    
+                    x1 = valor.getSaldo() - x
+                    valor.setSaldo(x1)
                     q = 0
                     for chave, valor in self.dicionario.items():
                         if chave == id_t:
                             q = 1
-                            valor.saldo = valor.saldo + x
+                            x1 = valor.getSaldo() + x
+                            valor.setSaldo(x1)
                             print("O valor foi transferido")
                     if q != 1:
-                        valor.saldo = valor.saldo + x
+                        x1 = valor.getSaldo + x
+                        valor.setSaldo(x1)
                         print("cliente não encontrado")
                 else:
                     print("Saldo insuficiente")      
         if p != 1:
             print("Cliente não encontrado")  
+
+class ADM:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
+
+    def getNomeA(self):
+        return self.nome
+    
+    def getSenhaA(self):
+        return self.senha
 
 
     
